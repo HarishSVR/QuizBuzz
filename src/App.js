187ce7159e42b1b140cbb598f068016cc3fcdcc3
HarishSVR <harishsvr@gmail.com>
2020-10-20 22:10:45 +0530

    quizdata

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Quiz from "./components/Quiz";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to QuizBuzz !!!</h1>
+      <h2>Welcome to QuizBuzz !!!</h2>
       <Quiz />
+      <span>
+        <button id="previous">Previous Question</button>
+        <button id="submit">Submit Quiz</button>
+        <button id="next">Next Question</button>
+        <button id="results">Show Answer</button>
+      </span>
     </div>
   );
 }
